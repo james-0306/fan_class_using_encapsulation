a868e229b19211d15d499826e2d2d3484de3e800
james malitan <jamesmalitan21@gmail.com>
2026-06-05 13:45:22 +0800

    adding the init of the attributes

--- fan_class.py
+++ fan_class.py
@@ -3,4 +3,10 @@ class Fan:
     SLOW = 1
     MEDIUM = 2
     FAST = 3
+    def __init__(self, speed = SLOW, radius = 5, color = "blue", on = True):
+        self.__speed = speed
+        self.__radius = radius
+        self.__color = color
+        self.__on = on
+
     
